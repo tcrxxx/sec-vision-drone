c2248debc0b9363adab43835ce2252df5cfe93cf
Thalles Rodrigues <tcarreiro.rodrigues@gmail.com>
2021-07-08 19:48:15 -0300

    (BROKEN) Inclusão do MTCNN para identificação de faces.

--- mainTello.py
+++ mainTello.py
@@ -1,11 +1,13 @@
+from faces import Faces
+from faces.Faces import *
 from tello.VideoCaptureTool import *
 import cv2
 
+# Tello
 w, h = 360, 240
 pid = [0.4, 0.4, 0]
 pError = 0
-startCounter = 1 # for no Flight 1   - for flight 0
-
+startCounter = 1  # for no Flight 1   - for flight 0
 myDrone = initializeTello()
 
 while True:
@@ -16,13 +18,15 @@ while True:
         startCounter = 1
 
     ## Step 1
-    img = telloGetFrame(myDrone, w, h)
+    #img = telloGetFrame(myDrone, w, h)
+    #img = telloGetFrame(myDrone, w, h)
     ## Step 2
-#    img, info = findFace(img)
+    # img, info = findFace(img)
+    execute(myDrone)
     ## Step 3
     #pError = trackFace(myDrone, info, w, pid, pError)
     # print(info[0][0])
-    cv2.imshow('Image', img)
+    #cv2.imshow('Image', img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         myDrone.land()
         break
